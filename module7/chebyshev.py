595a7dc862147ad58fbc49a7660277657b770fa6
Henrik <henrik@whitson.com>
2020-11-30 18:37:29 +0100

    Update chebyshev

--- module7/chebyshev.py
+++ module7/chebyshev.py
@@ -1,13 +1,24 @@
 import numpy as np
 
 
-def T(x, n=0):
+def T(x, n=0, recursive=False):
+    if recursive:
+        return _T_recursion(x, n)
+    else:
+        return _T(x, n)
+
+
+def _T(x, n):
+    return np.cos(n * np.arccos(x))
+
+
+def _T_recursion(x, n):
     if n == 0:
         return np.ones_like(x)
     elif n == 1:
         return x
     else:
-        return 2 * x * T(x, n - 1) - T(x, n - 2)
+        return 2 * x * _T_recursion(x, n - 1) - _T_recursion(x, n - 2)
 
 
 if __name__ == "__main__":
@@ -15,9 +26,21 @@ if __name__ == "__main__":
 
     x = np.linspace(-1, 1)
     N = int(input("N = "))
+
+    plt.figure(1)
     for n in range(N + 1):
         Tn = T(x, n)
         plt.plot(x, Tn, label=f"n = {n}")
 
+    plt.title("Explicit formula")
+    plt.grid()
+    plt.show()
+
+    plt.figure(2)
+    for n in range(N + 1):
+        Tn = T(x, n, recursive=True)
+        plt.plot(x, Tn, label=f"n = {n}")
+
+    plt.title("Recursive formula")
     plt.grid()
     plt.show()
