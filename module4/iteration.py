d72b23cfd606f81b953c3060f5adcf530547852f
Henrik <henrik@whitson.com>
2020-11-30 22:53:29 +0100

    update iteration.py

--- module4/iteration.py
+++ module4/iteration.py
@@ -34,6 +34,7 @@ def broyden(
 def fixed_point(
     x: np.ndarray, F: Callable, G: Callable, damp: float = 1.0, tol: float = 1e-8
 ) -> np.ndarray:
+
     iter = 0
     while lg.norm(F(x)) > tol and iter < 1e4:
         x = G(x)
@@ -50,12 +51,18 @@ if __name__ == "__main__":
     def J_F(x):
         return np.array([[2 * x[0] * x[1], x[0] ** 2], [5, np.cos(x[1])]])
 
-    x0 = np.array([0.1, 0.1])
-    print(x0)
-    x = quasi_newton(x0, F, J_F)
+    x0 = np.array([0.4, 0.1])
+    x = quasi_newton(x0, F, J_F, damp=1.4)
+    print(x)
+
+    x0 = np.array([0.4, 0.1])
+    x = quasi_newton(x0, F, J_F, damp=1.0)
+    print(x)
+
+    x0 = np.array([0.4, 0.1])
+    x = quasi_newton(x0, F, J_F, damp=0.9)
     print(x)
 
     x0 = np.array([0.1, 0.1])
-    print(x0)
     x = broyden(x0, F, J_F(x0))
     print(x)
